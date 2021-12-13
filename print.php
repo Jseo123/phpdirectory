566a9d165b17c117cd64b30d1a1e7cf10b97ac88
jseo123 <jose123135@gmail.com>
2021-12-13 21:13:41 +0100

    Print.php

---
+++ print.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h1>My first PHP script!<h1>";
+print "<h2>Hello World, this my second PHP string.<h2>";
+
+$a = array("Alina"=>"21", "Alexa"=>"24", "Ana"=>"18");
+print_r($a)
+
+?>
+
+</body>
+</html>
