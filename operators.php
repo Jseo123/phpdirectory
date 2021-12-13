7f492076125e8163aee68c4c7eb74bdb6db6bd54
jseo123 <jose123135@gmail.com>
2021-12-13 22:22:47 +0100

    Operators

---
+++ operators.php
@@ -0,0 +1,72 @@
+<?php
+$a = 2;
+$b = 5;
+
+var_dump($a == $b);
+echo "<br>";
+echo "the sum of $a + $b is = ";
+echo($a + $b);
+
+
+echo "<br>";
+echo "the substraction of $a - $b is = ";
+echo($a - $b);
+
+echo "<br>";
+echo "the multiplication of $a * $b is = ";
+echo($a * $b);
+
+echo "<br>";
+echo "the divition of $a / $b is = ";
+echo($a / $b);
+
+echo "<br>";
+echo "the modulus of $a % $b is = ";
+echo($a % $b);
+
+echo "<br>";
+echo "var_dump( $a != $b) <br>";
+var_dump( $a != $b);
+
+echo "<br>";
+echo "var_dump( $a < $b) <br>";
+var_dump( $a < $b);
+
+echo "<br>";
+echo "var_dump( $a > $b) <br>";
+var_dump( $a > $b);
+
+echo "<br>";
+echo "var_dump( $a <= $b) <br>";
+var_dump( $a <= $b);
+
+echo "<br>";
+echo "var_dump( $a >= $b) <br>";
+var_dump( $a >= $b);
+
+echo "<br>";
+echo "var_dump( $a && $b < 10) <br>";
+var_dump( $a && $b < 10);
+
+echo "<br>";
+echo "var_dump( $a and $b < 3) <br>";
+var_dump( $a and $b < 3);
+
+echo "<br>";
+echo "var_dump( $a || $b < 3) <br>";
+var_dump( $a || $b < 3);
+
+echo "<br>";
+echo "var_dump( $a or $b < 3) <br>";
+var_dump( $a or $b < 3);
+
+echo "<br>";
+echo "var_dump( $a xor $b < 3) <br>";
+var_dump( $a xor $b < 3);
+
+echo "<br>";
+$x = var_dump( $a or $b < 3);
+echo "$x = var_dump( $a or $b < 3);<br>
+echo(!$ x); <br>";
+print(!$x);
+
