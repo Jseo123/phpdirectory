ab0f195d6a6f079c371f3ab3ce9f80a122fad39b
jseo123 <jose123135@gmail.com>
2021-12-14 12:56:40 +0100

    Functions

---
+++ functions.php
@@ -0,0 +1,47 @@
+<?php
+function firstTry($a, $b){
+return $a + $b;
+};
+
+$c = firstTry(1, 2);
+echo $c;
+
+function multiply($a, $b){
+    return $a * $b;
+    };
+
+print "<br>";
+
+$d = multiply(2, 7);
+echo $d;
+
+function divide($a, $b){
+    return $a / $b;
+    };
+    echo("<br>");
+
+echo divide(10, 5);
+
+function operate($a, $op, $b){
+switch($op){
+    case "+":
+        return $a + $b;
+        break;
+    case "-":
+        return $a - $b;
+        break;
+    case "*";
+        return $a * $b;
+        break;
+    case "/":
+        return $a / $b;
+        break;
+        default:
+        return $a + $b;
+        break;
+}
+
+    };
+    echo("<br>");
+
+    echo operate(2, "*", 3 );
